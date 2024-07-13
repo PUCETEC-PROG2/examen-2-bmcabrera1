4e5408d61f7c6e2650eedaaa41cb49e3e80e66c8
Bryan <bcabrera2195@gmail.com>
2024-07-13 12:48:30 -0500

    Update

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=200, null=False)
+    gender = models.CharField(max_length=200, null=False)
+    directorName = models.CharField(max_length=200, null=False)
+    year = models.IntegerField(null=False)
+    sinopsis = models.TextField(max_length=2000)
+    
+    def __str__(self) -> str:
+        return self.title
