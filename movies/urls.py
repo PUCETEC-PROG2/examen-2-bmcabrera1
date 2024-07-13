b7e101d4bbfa8cdfbdbea812f4a78f81a842dbd8
Bryan <bcabrera2195@gmail.com>
2024-07-13 13:37:37 -0500

    Update

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("movie/<int:movie_id>/", views.Movie, name="movie"),
+    path("movie/<int:movie_id>/", views.movie, name="movie"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
 
-# Create your views here.
+def index(request):
+    movies = Movie.objects.order_by('year') 
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
+
+def movie(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    template = loader.get_template('display_movie.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
